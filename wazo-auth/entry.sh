36d05cd014b2206589b8d14276e9359e18bf0838
Francois Blackburn <fblackburn@wazo.community>
2021-04-13 14:16:48 -0400

    do not use deprecated acl_template

--- wazo-auth/entry.sh
+++ wazo-auth/entry.sh
@@ -23,7 +23,7 @@ TOKEN=$(curl -s -X POST -H 'Content-Type: application/json' -u "$CLI_USERNAME:$C
 USER_UUID=$( curl -s -X POST "http://localhost:9497/0.1/users" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{ \"password\": \"$PASSWORD\", \"purpose\": \"user\", \"username\": \"$USERNAME\", \"uuid\": \"$UUID\"}" -H "X-Auth-Token: $TOKEN" | jq -r .uuid )
 
 # set ACL for application bus event
-ACL_UUID=$( curl -s -X POST "http://localhost:9497/0.1/policies"  -H  "accept: application/json" -H  "Content-Type: application/json" -d "{ \"acl_templates\": [\"events.applications.#\"], \"description\": \"application events\", \"name\": \"application_events\" }"  -H "X-Auth-Token: $TOKEN" | jq -r .uuid )
+ACL_UUID=$( curl -s -X POST "http://localhost:9497/0.1/policies"  -H  "accept: application/json" -H  "Content-Type: application/json" -d "{ \"acl\": [\"events.applications.#\"], \"description\": \"application events\", \"name\": \"application_events\" }"  -H "X-Auth-Token: $TOKEN" | jq -r .uuid )
 curl -X PUT "http://localhost:9497/0.1/users/$USER_UUID/policies/$ACL_UUID" -H  "accept: application/json" -H  "Content-Type: application/json" -d "" -H "X-Auth-Token: $TOKEN"
 
 while true; do
